864889ecdb17a38e091799aa958177fca32b1d90
kstrack-grose <kiristrackgrose@gmail.com>
2015-07-09 12:30:26 -0700

    add multiple unicorns and resize to interact

--- src/tmpDancer.js
+++ src/tmpDancer.js
@@ -48,11 +48,25 @@ var makeUnicornDancer = function(top, left, timeBetweenSteps){
   makeDancer.apply(this, arguments);
   //this.top = top;
   //this.left = left;
-  this.face = 'lib/assets/img/unicorn.png';
+  this.faces = [  {face:    'lib/assets/img/unicorn.png',
+                   height: 180,
+                   width: 150},
+                   {face: 'lib/assets/img/derpyUnicorn.png',
+                    height: 200,
+                    width: 140},
+                   {face: 'lib/assets/img/flyingUnicorn.png',
+                    height: 143,
+                    width: 190}
+                  ];
+  this.idx = Math.floor(Math.random()*this.faces.length);
+  this.face = this.faces[this.idx]["face"];
+  this.height = this.faces[this.idx]["height"];
+  this.width = this.faces[this.idx]["width"];
   //'http://www.wallmonkeys.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d
   // 27136e95/c/a/ca-301-v04-unicorn_1.png';
-  this.$node.append('<img src="' + this.face + '" height = "180px" width = "150px">');
 
+  this.$node.append('<img src="' + this.face + '" height = "' + this.height + 'px" width = "' + this.width + 'px">');
+  //debugger;
 };
 
 makeUnicornDancer.prototype = Object.create(makeDancer.prototype);
@@ -65,6 +79,9 @@ makeUnicornDancer.prototype.step = function(){
   this.$node.animate({top: this.top + 100 + "px", border: 0}, 200);
 };
 makeUnicornDancer.prototype.interact = function() {
-  this.$node.toggleClass('flipped', 800);
+  //this.$node.toggleClass('flipped', 800);
+  //debugger;
+  this.$node.children("img:first").animate({height: 2*this.height+"px", width: 2*this.width+"px"}, 200);
+  this.$node.children("img:first").animate({height: this.height+"px", width: this.width+"px"}, 200);
 };
 
